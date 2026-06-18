c041bbc516bd51001abf7068df6875cb727d012b
Romylussone <romylussone@gmail.com>
2026-06-18 15:52:27 +0000

    feat: :balloon: dml checkpoint

---
+++ checkpoints/dml-project/manipulation-checkpoint.sql
@@ -0,0 +1,23 @@
+-- =========================
+-- PRODUCT
+-- =========================
+INSERT INTO Product (Product_id, Product_Name, Category, Price)
+VALUES
+('P01', 'Samsung Galaxy S20', 'Smartphone', 3299),
+('P02', 'ASUS Notebook', 'PC', 4599);
+
+-- =========================
+-- CUSTOMER
+-- =========================
+INSERT INTO Customer (Customer_id, Customer_Name, Customer_Tel)
+VALUES
+('C01', 'ALI', '71321009'),
+('C02', 'ASMA', '77345823');
+
+-- =========================
+-- ORDERS
+-- =========================
+INSERT INTO Orders (Customer_id, Product_id, OrderDate, Quantity, Total_Amount)
+VALUES
+('C01', 'P02', NULL, 2, 9198),
+('C02', 'P01', '2020-05-28', 1, 3299);
